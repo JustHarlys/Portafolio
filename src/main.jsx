e47126f47341204d0d7bf74adbb42084cf57aca8
JustHarlys <harlysmc@gmail.com>
2025-08-24 13:19:07 -0400

    M

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import DarkToggleProvider from './Context/DarkToggleContext.jsx'
 import DevPage from './pages/DevPage.jsx'
+import {ReactLenis} from 'lenis/react'
 
 createRoot(document.getElementById('root')).render(
-
-    <DarkToggleProvider>
-        <DevPage />
-    </DarkToggleProvider>
+    <ReactLenis root>
+        <DarkToggleProvider>
+            <DevPage />
+        </DarkToggleProvider>
+    </ReactLenis>
 )
